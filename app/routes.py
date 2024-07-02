aa287cdc9080472215ecc16975cfb542b8153e5f
Anthony Carmona <acarmon2@ucsc.edu>
2024-07-02 18:37:57 +0000

    further updating the layour of the Flask application

---
+++ app/routes.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from twitch_api import generate_headers, get_user_data, update_sql
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    user_name = request.form.get('search') 
+    headers = generate_headers()
+    user_data = get_user_data(user_name, headers)
+    if user_data:
+        videos = user_data['video_data'][:5]
+        return render_template('results.html',
+                            streamer=user_data['name'],
+                            description=user_data['description'],
+                            followers=format(user_data['follower_count'], ","),
+                            broadcaster_type=user_data['broadcaster_type'],
+                            last_played=user_data['last_game_played'],
+                            created_at=user_data['created_at'],
+                            img_src=user_data['pfp_url'],
+                            videos=videos)
+    else:
+        return render_template('results.html', streamer=None, description=None)
+
+@app.route('/add', methods=['POST'])
+def add():
+    user_name = request.form.get('search')
+    headers = generate_headers()
+    user_data = get_user_data(user_name, headers)
+    if user_data:
+        
+        update_sql([user_data])
+        return render_template('add.html', user_data=user_data)
+    else:
+        return render_template('add.html', user_data=None)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
--- app/twitch.py
+++ app/twitch.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import pandas as pd
 import requests
 import sqlalchemy as db
-import twitchAPI
 
 CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('TWITCH_CLIENT_SECRET')
@@ -26,7 +25,6 @@ def generate_headers():
     })
 
     auth_response_data = auth_response.json()
-    print(auth_response_data)
     access_token = auth_response_data['access_token']
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token),
@@ -48,10 +46,14 @@ def get_user_data(user_name, headers):
     user_data = user_req.json()['data']
 
     if len(user_data) != 0:
-        user_data = user_req.json()['data'][0]
+        user_data = {}
+        user_data_json = user_req.json()['data'][0]
 
-        user_id = user_data['id']
-
+        user_id = user_data_json['id']
+        user_data['name'] = user_data_json['display_name']
+        user_data['description'] = user_data_json['description']
+        user_data['broadcaster_type'] = user_data_json['broadcaster_type']
+        user_data['pfp_url'] = user_data_json['profile_image_url']
         # Requests the follower data of the user and stores it in the user data
         followers_req = requests.get(
                                         BASE_URL +
@@ -76,43 +78,12 @@ def get_user_data(user_name, headers):
                                     user_id,
                                     headers=headers)
         user_data['video_data'] = video_req.json()['data']
-
+        user_data['created_at'] =  datetime.strptime(
+        user_data_json['created_at'], "%Y-%m-%dT%H:%M:%SZ"
+    ).strftime("%B %d, %Y")
         return user_data
     else:
         return None
-
-
-def user_to_str(user_data):
-    """
-    Returns a string represenation of a Twitch user's data,
-    given a dictionary
-    """
-    user_str = "Name: " + user_data['display_name'] + \
-        "\nDescription: " + user_data['description'] + \
-        "\nFollower count: " + format(user_data['follower_count'], ",") + \
-        "\nBroadcaster Type: " + user_data['broadcaster_type'] + \
-        "\nMember since: " + datetime.strptime(
-        user_data['created_at'], "%Y-%m-%dT%H:%M:%SZ"
-    ).strftime("%B %d, %Y") + \
-        "\nLast played: " + user_data['last_game_played'] + \
-        "\nMost Recent Videos:"
-
-    # Prints out information on the 5 (at most) most recent videos
-    i = 0
-    while i < 5 and i < len(user_data['video_data']):
-        curr_vid = user_data['video_data'][i]
-        user_str += "\n\nTitle: " + curr_vid['title'] + \
-            "\nViews: " + format(curr_vid['view_count'], ",") + \
-            "\nPosted: "
-        vid_date = curr_vid['published_at']
-        try:
-            vid_date = datetime.strptime(vid_date, "%Y-%m-%dT%H:%M:%S.%fZ")
-        except ValueError:
-            vid_date = datetime.strptime(vid_date, "%Y-%m-%dT%H:%M:%SZ")
-        user_str += vid_date.strftime("%B %d, %Y")
-        i += 1
-
-    return user_str
 
 
 def update_sql(user_data_list):
@@ -134,35 +105,3 @@ def update_sql(user_data_list):
             query_result = connection.execute(db.text(query)).fetchall()
             print(pd.DataFrame(query_result))
     print('Bye!')
-
-
-def main():
-    headers = generate_headers()
-    # Setup engine for SQL interaction
-
-    # Asks user to input username of a Twitch account
-    user_name = ''
-
-    user_data_list = []  # Used to create a list of user data
-
-    while True:
-        user_name = input('Enter username to search and add or QUIT: ')
-        print('------------------')
-
-        if user_name.upper() == 'QUIT':
-            break
-        user_data = get_user_data(user_name.strip(), headers)
-        if user_data:
-            print("User found!")
-            if user_data not in user_data_list:
-                user_data_list.append(user_data)
-            print(user_to_str(user_data))
-        else:
-            print("User not found :(")
-        print('------------------')
-
-    update_sql(user_data_list)
-
-
-if __name__ == '__main__':
-    main()
